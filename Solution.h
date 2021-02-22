3646ee64f4594bdafcf48990a2fc93640bf41793
Y-Sau <79433831+Y-Sau@users.noreply.github.com>
2021-02-22 06:55:49 +0300

    Add files via upload

---
+++ Solution.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+ public:
+  int singleNumber(vector<int>& nums) {
+      int tmp = nums[0];
+      for (int i = 1; i < nums.size(); i++) {
+          tmp = tmp xor nums[i];   
+      }
+      return tmp;
+  }
+};
